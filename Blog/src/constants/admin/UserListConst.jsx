43e401c01946bf47724c6640080c361be3f8a991
YeYint3424 <xino3424@gmail.com>
2024-01-19 13:31:40 +0630

    [add] user role update model box

--- Blog/src/constants/admin/UserListConst.jsx
+++ Blog/src/constants/admin/UserListConst.jsx
@@ -2,13 +2,13 @@ import { editButton } from "../../components/common/CommonButton";
 
 export const tableTitle = ["Username", "Email", "Date In", "Status", ""];
 
-export const tableBody = (fetchData) => {
+export const tableBody = (fetchData, setAppearModel) => {
   return fetchData.map((data) => ({
     name: data.name,
     email: data.email,
     date: data.date,
     status: <UserStatusColor status={data.status} />,
-    edit: editButton({ id: data.id }),
+    edit: editButton({ id: data.id ,setAppearModel}),
   }));
 };
 
--- Blog/src/pages/admin/UserList.jsx
+++ Blog/src/pages/admin/UserList.jsx
@@ -1,53 +1,65 @@
+import { useState } from "react";
 import CommonTable from "../../components/common/CommonTable";
-import { tableBody, tableTitle } from "../../constants/admin/UserListConst";
+import {
+  modelLabel,
+  tableBody,
+  tableTitle,
+} from "../../constants/admin/UserListConst";
+import CommonModel from "../../components/common/CommonModel";
 
 const UserList = () => {
-    const fetchData = [
-        {
-          id: "1",
-          name: "Mg Mg",
-          date: "Dec 18 2023",
-          email: "yy@gmail.com",
-          status: "Active",
-        },
-        {
-          id: "2",
-          name: "Mg YE Yint",
-          date: "Dec 1 2023",
-          email: "yy@gmail.com",
-          status: "Inactive",
-        },
-        {
-          id: "3",
-          name: "Muuuu",
-          date: "Dec 1 2023",
-          email: "yy@gmail.com",
-          status: "Inactive",
-        },
-      ];
-      const tbody = tableBody(fetchData);
-      const bodyData = () =>
-        tbody.map((data, index) => (
-          <tr key={index}>
-            <td className="d-flex align-items-center">
-              <img
-                src="https://i.imgflip.com/73u9iv.jpg"
-                alt="user"
-                width={40}
-                height={40}
-              />
-              <span className="mx-5 bold">{data.name}</span>
-            </td>
-            <td>{data.date}</td>
-            <td>{data.email}</td>
-            <td>{data.status}</td>
-            <td>{data.edit}</td>
-          </tr>
-        ));
-      
-    return ( 
-        <CommonTable titles={tableTitle} bodyData={bodyData()}/>
-     );
-}
- 
+  const [appearModel, setAppearModel] = useState(false);
+  const fetchData = [
+    {
+      id: "1",
+      name: "Mg Mg",
+      date: "Dec 18 2023",
+      email: "yy@gmail.com",
+      status: "Active",
+    },
+    {
+      id: "2",
+      name: "Mg YE Yint",
+      date: "Dec 1 2023",
+      email: "yy@gmail.com",
+      status: "Inactive",
+    },
+    {
+      id: "3",
+      name: "Muuuu",
+      date: "Dec 1 2023",
+      email: "yy@gmail.com",
+      status: "Inactive",
+    },
+  ];
+  const tbody = tableBody(fetchData, setAppearModel);
+  const bodyData = () =>
+    tbody.map((data, index) => (
+      <tr key={index}>
+        <td className="d-flex align-items-center">
+          <img
+            src="https://i.imgflip.com/73u9iv.jpg"
+            alt="user"
+            width={40}
+            height={40}
+          />
+          <span className="mx-5 bold">{data.name}</span>
+        </td>
+        <td>{data.date}</td>
+        <td>{data.email}</td>
+        <td>{data.status}</td>
+        <td>{data.edit}</td>
+      </tr>
+    ));
+
+  return (
+    <>
+      {appearModel && (
+        <CommonModel modelLabel={modelLabel} setAppearModel={setAppearModel} />
+      )}
+      <CommonTable titles={tableTitle} bodyData={bodyData()} />
+    </>
+  );
+};
+
 export default UserList;
